38b03c3864ac7d4e5e558c66ca62ee50775780a0
plasticine9750 <975036719@qq.com>
2023-01-01 17:39:13 +0800

    feat(server): update server template

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,45 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CacheModule, CacheStore, Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { redisStore } from 'cache-manager-redis-store'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+import { UserModule } from './user/user.module'
+
+import { loadConfig } from './utils'
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      async useFactory() {
+        const redisConfig = loadConfig('REDIS_CONFIG')
+        const { host, port, username, password, database } = redisConfig
+        const store = await redisStore({
+          socket: {
+            host,
+            port,
+          },
+          username,
+          password,
+          database,
+        })
+
+        return {
+          store: store as unknown as CacheStore,
+          ttl: 15,
+        }
+      },
+    }),
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [loadConfig],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
