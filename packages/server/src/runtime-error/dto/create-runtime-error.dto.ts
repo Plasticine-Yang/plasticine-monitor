6ce923f339e003a990038953f08499e859119d6b
plasticine9750 <975036719@qq.com>
2023-01-03 21:43:27 +0800

    chore(server): remove id field in CreateRuntimeErrorDto

--- packages/server/src/runtime-error/dto/create-runtime-error.dto.ts
+++ packages/server/src/runtime-error/dto/create-runtime-error.dto.ts
@@ -2,9 +2,6 @@ import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty } from 'class-validator'
 
 export class CreateRuntimeErrorDto {
-  @ApiProperty({ example: 1 })
-  id: number
-
   @ApiProperty({ example: 'Uncaught ReferenceError: a is not defined' })
   @IsNotEmpty()
   errorMessage: string
