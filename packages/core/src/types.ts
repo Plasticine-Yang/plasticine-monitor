817d8e1fa221409cd3a6872745bf347e9e7ff47d
plasticine9750 <975036719@qq.com>
2023-01-10 20:56:01 +0800

    feat(core): add plugin system

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,12 +1,7 @@
 import { Core } from './core'
 
-interface CoreConfig {
-  /** @description 监控平台 url */
-  platformURL?: string
-}
-
 interface Plugin {
   install: (instance: Core) => void
 }
 
-export type { CoreConfig, Plugin }
+export type { Plugin }
