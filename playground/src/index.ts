b8ab87d7cd903aa5d971b6c96c9927853b13bd43
plasticine9750 <975036719@qq.com>
2023-01-18 11:23:40 +0800

    chore(playground): update playground

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,23 +1,31 @@
+import './style.css'
+
 import { WebSDK } from '@plasticine-monitor/browser'
 
 new WebSDK({
   senderURL: `${new URL(import.meta.url).origin}/api`,
 })
 
-const $root = document.getElementById('root')!
-
-$root.innerHTML = `
-<button id="btn">emit error</button>
-<button id="pbtn">emit promise error</button>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="btn-group">
+    <button id="btn-js-error">emit js error</button>
+    <button id="btn-promise-error">emit promise error</button>
+  </div>
 `
 
-document.getElementById('btn')?.addEventListener('click', () => {
-  // @ts-ignore
-  undefinedFn()
-})
-
-document.getElementById('pbtn')?.addEventListener('click', () => {
-  new Promise((_, reject) => {
-    reject('promise error')
+// trigger js error
+document
+  .querySelector<HTMLButtonElement>('#btn-js-error')!
+  .addEventListener('click', () => {
+    // @ts-ignore
+    undefinedFn()
   })
-})
+
+// trigger promise error
+document
+  .querySelector<HTMLButtonElement>('#btn-promise-error')!
+  .addEventListener('click', () => {
+    new Promise((_, reject) => {
+      reject('promise error')
+    })
+  })
