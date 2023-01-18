9c9e7459cc1bf4c30ba894c807450e3b0cc03803
plasticine9750 <975036719@qq.com>
2023-01-18 15:16:04 +0800

    feat(playground): add production mode

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,16 +2,15 @@ import { defineConfig } from 'vite'
 import dotenv from 'dotenv'
 import { resolve } from 'path'
 
-const NODE_ENV = process.env.NODE_ENV ?? 'development'
+const PROXY_MODE = process.env.PROXY_MODE ?? 'development'
 dotenv.config({
   path: resolve(
     process.cwd(),
-    `.env${NODE_ENV === 'development' ? '' : `.${NODE_ENV}`}`,
+    `.env${PROXY_MODE === 'development' ? '' : `.${PROXY_MODE}`}`,
   ),
 })
 
 const { PROXY_URL = 'http://localhost:3000/v1' } = process.env
-console.log(PROXY_URL)
 
 export default defineConfig({
   server: {
@@ -19,7 +18,7 @@ export default defineConfig({
       '/api': {
         target: PROXY_URL,
         changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
