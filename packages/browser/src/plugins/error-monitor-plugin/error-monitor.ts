3e26cabc3733c6ace3b25effb2420e6bcbcf3a23
plasticine9750 <975036719@qq.com>
2023-01-18 11:04:11 +0800

    feat(browser): support monitor promise error

--- packages/browser/src/plugins/error-monitor-plugin/error-monitor.ts
+++ packages/browser/src/plugins/error-monitor-plugin/error-monitor.ts
@@ -4,8 +4,12 @@ import { ReportErrorPayload } from '@plasticine-monitor/shared'
 class ErrorMonitor {
   constructor(private webSDK: WebSDK) {
     this.initJSErrorMonitor()
+    this.initPromiseErrorMonitor()
   }
 
+  /**
+   * @description 监控 JS Error
+   */
   initJSErrorMonitor() {
     window.addEventListener('error', (ev) => {
       // 生成错误上报 payload
@@ -14,6 +18,22 @@ class ErrorMonitor {
         message: ev.message,
         uid: this.genErrorUid(`${ev.type}-${ev.message}`),
         stacktrace: 'stack',
+      }
+
+      this.webSDK.sender.send('report-error', payload)
+    })
+  }
+
+  /**
+   * @description 监控 Promise Error
+   */
+  initPromiseErrorMonitor() {
+    window.addEventListener('unhandledrejection', (ev) => {
+      // 生成错误上报 payload
+      const payload: ReportErrorPayload = {
+        type: ev.type,
+        message: ev.reason,
+        uid: this.genErrorUid(`${ev.type}-${ev.reason}`),
       }
 
       this.webSDK.sender.send('report-error', payload)
