15185bafab87462b4f52b987c4d4ad3278a13c9e
yangwenfeng.0719 <yangwenfeng.0719@bytedance.com>
2023-02-19 13:12:16 +0800

    feat(playground): html title 区分 sentry 和 plasticine-monitor

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,5 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+
+import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 import { resolve } from 'path'
 
@@ -10,24 +12,35 @@ const shouldMock = Boolean(process.env.SHOULD_MOCK) ?? false
 const proxyUrl = shouldMock ? 'https://mock.apifox.cn/m1/2144205-0-default' : 'http://localhost:3000/v1'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': resolve(BASE_DIR, 'components'),
-      '@utils': resolve(BASE_DIR, 'utils'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: proxyUrl,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: mode === 'sentry' ? 'Sentry Playground' : 'Plasticine Monitor Playground',
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@components': resolve(BASE_DIR, 'components'),
+        '@utils': resolve(BASE_DIR, 'utils'),
       },
     },
-  },
-  build: {
-    sourcemap: true,
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: proxyUrl,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    build: {
+      sourcemap: true,
+    },
+  }
 })
