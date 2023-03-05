a6781860a476f252417d7c31e1085526a6657ad9
plasticine <975036719@qq.com>
2023-03-05 16:23:09 +0800

    chore: vitest 配置调整

--- packages/core/test/mock/test-client.ts
+++ packages/core/test/mock/test-client.ts
@@ -1,4 +1,4 @@
-import type { ClientOptions } from '@plasticine-monitor/types'
+import { type ClientOptions, type Event, EventLevel, type RuntimeException } from '@plasticine-monitor/types'
 
 import { BaseClient } from '../../src/base-client'
 
@@ -7,5 +7,25 @@ export type TestClientOptions = ClientOptions
 export class TestClient extends BaseClient<TestClientOptions> {
   constructor(options: TestClientOptions) {
     super(options)
+  }
+
+  public eventFromException(exception: RuntimeException): Event {
+    return {
+      eventId: '',
+      exception: {
+        type: '',
+        value: '',
+      },
+      level: EventLevel.Info,
+      message: exception.message,
+    }
+  }
+
+  public eventFromMessage(message: string, level?: EventLevel | undefined): Event {
+    return {
+      eventId: '',
+      level,
+      message,
+    }
   }
 }
