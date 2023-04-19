a8fa55eb7973b27e88a9f3a8a89f7988571cca41
plasticine <975036719@qq.com>
2023-04-20 00:49:05 +0800

    chore: update

--- playground/src/setup/react/utils/emit-error/js-error.ts
+++ playground/src/setup/react/utils/emit-error/js-error.ts
@@ -3,8 +3,6 @@ import { markFuncStack } from './shared'
 export const emitJSError = () => {
   markFuncStack()
 
-  setTimeout(async () => {
-    // @ts-ignore
-    undefinedFn()
-  }, 300)
+  // @ts-ignore
+  undefinedFn()
 }
